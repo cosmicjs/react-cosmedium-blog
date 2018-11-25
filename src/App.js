d8a7030fb52760db37563165d6cb730dc4c1ef1c
3ndrius <andrzejo1991@gmail.com>
2018-11-25 18:41:41 +0100

    Initialize cosmicjs and first data recieved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-
-
+const Cosmic = require('cosmicjs');
 
 class App extends Component {
+
+  componentDidMount = () => {
+    var api = new Cosmic();
+    var bucket = api.bucket({
+      slug:'b20d8c20-f0d6-11e8-a47e-3bb5370873e3'
+    })
+    bucket.getObjects().then(data => {
+     console.log(data);
+    
+    
+    }).catch(err => {
+      console.log(err)
+    })
+
+  }
   render() {
     return (
       <div className="App">
