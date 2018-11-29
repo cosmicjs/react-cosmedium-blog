68cc70d6fc086a48bf75d2f3deb0b93b86a5a00b
3ndrius <andrzejo1991@gmail.com>
2018-11-29 23:45:09 +0100

    Adding style to single-post component

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
+import '../css/SinglePost.css';
 
  function SinglePost(props) {
-     console.log("Router props", props.match.params.post_slug);
-     console.log("map state post", props.post);
-    
+
   return (
-    <div>
-    {props.post && <h3>{props.post.metadata.category.slug} </h3> }
+    <div className="page-container">
+     <div className="page-wrap">
+     {props.post && <h3>{props.post.metadata.category.slug} </h3> }
      {props.post && <h1>{props.post.title}</h1> }
-     {props.post && <img src={props.post.metadata.image.url} alt=""/> }
-     {props.post && <h5>{props.post.metadata.snipped}</h5> }
-     {props.post && <p dangerouslySetInnerHTML={{__html:props.post.content}}></p> }
+     {props.post && <h4>{props.post.metadata.snipped}</h4> }
+     </div>
+     {props.post && <img className="figure" src={props.post.metadata.image.url} alt=""/>  }
+     {props.post && <p className="text-page" dangerouslySetInnerHTML={{__html:props.post.content}}></p> }
+     <div className="side-bar">vote</div>
     </div>
   )
 }
