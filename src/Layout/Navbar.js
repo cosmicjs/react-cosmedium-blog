50236d2e1d22274adfcd39f591c66eb6f364d28a
3ndrius <andrzejo1991@gmail.com>
2018-12-02 19:48:06 +0100

    Adding search

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
-
- function Navbar(props) {
-    console.log("Search", props.search);
+import SearchList from '../components/SearchList';
+class Navbar extends Component {
+  
+render(){
   return (
  <div className="wrap-to-border">
         <div className="navbar-wrap">
@@ -12,8 +13,9 @@ import Input from '../components/Input';
         </div>
         <div className="right-side">
             <div className="search-wrap">
-                <i className="fa fa-search fa-lg" aria-hidden="true"></i>
-               <Input />
+                <i className="fa fa-search fa-lg" aria-hidden="true" ></i>
+            <Input />
+               <SearchList />
              </div>
             <Link to="/signin" className="signin">Sign in</Link>
             <button className="btn-join">Get started</button>
@@ -21,6 +23,7 @@ import Input from '../components/Input';
     </div>
  </div>
   )
+}
 }
 
 export default(Navbar);
---
+++ src/components/SearchList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+import '../css/SearchList.css'
+ function SearchList(props) {
+     console.log("Search list" , props.lists);
+  return (
+    <div className="search-list"> 
+    <span className="box"></span>
+         {
+           props.value && props.lists.map((item, index) => {
+                 return(
+                    <Link key={index} to={'/' + item.slug}>
+                    <div className="it">{item.title}</div>
+                    </Link> 
+                 )
+             })
+         }
+      </div>
+  )
+}
+const mapStateToProps = (state) => {
+    return{
+        lists: state.posts.newList,
+        value: state.posts.value
+     
+    }
+}
+
+export default connect(mapStateToProps)(SearchList);
